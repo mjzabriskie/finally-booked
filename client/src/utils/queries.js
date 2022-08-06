824477d825b0e35e2437366ebfccfa7e760208f9
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-06 13:13:35 -0600

    added queries.js, switching to front-end branch

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          description
+          bookId
+          image
+          link
+          title
+        }
+      }
+`
